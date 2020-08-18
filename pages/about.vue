5d8ef3593c14505c7841c55daebea2bd225c76b4
Sam Jackson <samuelsjackson7@gmail.com>
2020-08-18 16:08:18 -0400

    Created an about page with header containing title and meta tags

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an app that displays dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About The App", // This changes the title on the tab
+      meta: [
+        {
+          hid: "description", // unique identifier
+          name: "description", // actual meta tag
+          content: "Best place for corny dad jokes", // content of the meta tag
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
