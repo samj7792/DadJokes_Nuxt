05e5b73160b0348eecdd04d04e358709e76a6ff3
Sam Jackson <samuelsjackson7@gmail.com>
2020-08-18 19:00:57 -0400

    can now search jokes

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <SearchJokes v-on:search-text="searchText" />
     <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
   </div>
 </template>
@@ -7,9 +8,10 @@
 <script>
 import axios from "axios";
 import Joke from "../../components/Joke"; // not necessary as components are auto imported
+import SearchJokes from "../../components/SearchJokes";
 
 export default {
-  components: { Joke }, // again, components are auto imported
+  components: { Joke, SearchJokes }, // again, components are auto imported
   data() {
     return {
       jokes: [],
@@ -29,6 +31,26 @@ export default {
     } catch (error) {
       console.log(error);
     }
+  },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: "application/json",
+        },
+      };
+
+      try {
+        const res = await axios.get(
+          `https://icanhazdadjoke.com/search?term=${text}`,
+          config
+        );
+
+        this.jokes = res.data.results;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
   head() {
     return {
